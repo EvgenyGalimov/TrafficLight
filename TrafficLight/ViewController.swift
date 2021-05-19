0ea33434ffef1f5ce25fbea8832e2bcb47664cfa
Evgeny <skft80@icloud.com>
2021-05-19 17:12:17 +0200

    added BusinessLogic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -15,20 +15,58 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
+    
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var greenLight: UIView!
-    private var actualLight = ActualLight.red
-    private let lightOn: Double = 1
-    private let lightOff: Double = 0.35
+    private var actualLight: ActualLight?
+    private let lightOn: CGFloat = 1
+    private let lightOff: CGFloat = 0.2
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
    
+     
     @IBAction func tappedStartButton() {
+        nextLight()
+        }
+    
+    func nextLight() {
         
+        switch actualLight {
+        case .red:
+            UIView.animate(withDuration: 0.5) {
+                self.redLight.alpha = self.lightOff
+                self.yellowLight.alpha = self.lightOn
+                self.greenLight.alpha = self.lightOff
+            }
+            actualLight = .yellow
+            
+        case .yellow:
+            UIView.animate(withDuration: 0.5) {
+                self.redLight.alpha = self.lightOff
+                self.yellowLight.alpha = self.lightOff
+                self.greenLight.alpha = self.lightOn
+            }
+            actualLight = .green
+            
+        case .green:
+            UIView.animate(withDuration: 0.5) {
+                self.redLight.alpha = self.lightOn
+                self.yellowLight.alpha = self.lightOff
+                self.greenLight.alpha = self.lightOff
+            }
+            actualLight = .red
+            
+        case .none:
+            redLight.alpha = lightOn
+            actualLight = .red
+            startButton.setTitle("NEXT", for: .normal)
+        }
     }
     
 }
+    
+
     
 
