f7722164d1720bc08fa458235b0c32bc11673197
Evgeny <skft80@icloud.com>
2021-05-19 10:57:26 +0200

    Added mainScreen

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
+enum ActualLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
+
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var greenLight: UIView!
+    private var actualLight = ActualLight.red
+    private let lightOn: Double = 1
+    private let lightOff: Double = 0.35
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+   
+    @IBAction func tappedStartButton() {
+        
+    }
+    
 }
+    
 
